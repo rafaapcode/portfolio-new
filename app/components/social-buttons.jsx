ddfac4bd413ba289a485707b1180f3b4f4b64cfc
rafaapcode <rafaap2003@gmail.com>
2023-05-22 08:58:16 -0300

    feat: begin the qualification section

--- app/components/social-buttons.jsx
+++ app/components/social-buttons.jsx
@@ -6,9 +6,9 @@ export default function SocialButton({ imgUrl, text }) {
   return (
     <a href="https://www.linkedin.com/in/rafael-ap/" target="_blank">
       <motion.button
-        initial={{ x: -530 }}
-        animate={{ x: 0 }}
-        transition={{duration: 0.7}}
+        initial={{ scaleY: 0 }}
+        animate={{ scaleY: 1 }}
+        transition={{duration: 0.2}}
         className="border border-slate-700 flex px-5 py-2 rounded-lg shadow shadow-slate-500/50 hover:bg-[#201f1f] transition-all duration-75"
       >
         <Image src={imgUrl} width={25} height={25} alt="logo-social" />
---
+++ app/qualification/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Title from "../components/title";
+
+export default function Formacao(){
+    return (
+        <section className="w-full lg:w-[70%]  h-[80%] lg:h-screen md:h-screen px-5 overflow-auto">
+            <Title title='Formação' />
+            <div className="mt-10 w-full flex flex-col justify-center items-center gap-y-1">
+                <h1>tes</h1>
+            </div>
+        </section>
+    );
+}
