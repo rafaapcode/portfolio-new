3a3298f89150e69c1a43261cf4ae44ab637aece8
rafaapcode <rafaap2003@gmail.com>
2023-05-23 09:31:43 -0300

    feat: qualification section complete

--- app/components/card-qualification.jsx
+++ app/components/card-qualification.jsx
@@ -4,29 +4,21 @@ export default function CardQualification({
   curso,
   instituicao,
   duracao,
-  desc,
   transitionDuration,
 }) {
   return (
-    <div className="w-full">
+    <div className="flex h-fit w-full md:w-fit lg:w-fit">
       <motion.article
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 1 }}
-        transition={{ duration: 0 }}
-        className="w-full px-2 py-3 border border-slate-500 rounded-lg"
+        transition={{ duration: transitionDuration }}
+        className="h-[15%] px-2 py-3 border border-slate-500 rounded-lg"
       >
-        <div className="w-full flex items-center gap-x-5 mb-2">
-          <h2 className="text-base md:text-lg lg:text-lg">
-            Sou um card - <b>SENAI</b>
-          </h2>
-          <small className="text-[#666565]">Jun 2022 - Dez 2022</small>
-        </div>
-        <p className="text-sm md:text-base lg:text-base text-[#666565]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos nobis,
-          nihil dolor voluptate itaque consequatur dicta recusandae dignissimos
-          doloremque, non vero provident iste quis consectetur, dolore soluta
-          illo eum tempora?
-        </p>
+        <h2 className="text-base md:text-lg lg:text-lg">
+          {curso} 
+        </h2>
+        <p className="underline underline-offset-4"><b>{instituicao}</b></p>
+        <small className="text-[#666565]">{duracao}</small>
       </motion.article>
     </div>
   );
--- app/qualification/page.jsx
+++ app/qualification/page.jsx
@@ -4,13 +4,46 @@ import Title from "../components/title";
 import CardQualification from "../components/card-qualification";
 
 export default function Formacao() {
-  const qualification = [{}];
+  const qualification = [
+    {
+      curso: "Técnico em Redes de Computadores",
+      instituicao: "SENAI",
+      duracao: "Jan 2020 - Jul 2021",
+    },
+    {
+      curso: "Azure AI Fundamentals",
+      instituicao: "SENAI",
+      duracao: "Jul 2022 - Nov 2022",
+    },
+    {
+      curso: "Programador Front-End",
+      instituicao: "SENAI",
+      duracao: "Jun 2022 - Nov 2022",
+    },
+    {
+      curso: "Google Cloud Foundations",
+      instituicao: "SENAI",
+      duracao: "Fev 2023 - Mai 2023",
+    },
+    {
+      curso: "Engenharia de Software",
+      instituicao: "Uninter",
+      duracao: "Fev 2022 - Atual",
+    },
+  ];
 
   return (
-    <section className="w-full lg:w-[70%] h-[80%] lg:h-screen md:h-screen px-5 overflow-auto">
+    <section className="w-full lg:w-[70%] h-[80%] lg:h-screen md:h-screen px-5">
       <Title title="Formação" />
-      <div className="mt-10 w-full md:w-[75%] lg:w-[70%] flex flex-col justify-center items-center gap-y-1">
-        <CardQualification />
+      <div className="mt-10 w-full h-[63%] lg:h-fit md:h-fit flex  flex-wrap gap-y-3 gap-x-3 overflow-auto">
+        {qualification.map(({ curso, instituicao, duracao }, index) => (
+          <CardQualification
+            curso={curso}
+            instituicao={instituicao}
+            duracao={duracao}
+            transitionDuration={index / 10}
+          />
+        ))}
       </div>
     </section>
   );
