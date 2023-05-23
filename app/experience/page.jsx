cbaa9270d4bf926806fa325ef3f75e12fffdbdf6
rafaapcode <rafaap2003@gmail.com>
2023-05-23 09:43:13 -0300

    feat: begin the experience section

---
+++ app/experience/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Title from "../components/title";
+
+export default function Experience() {
+
+
+  return (
+    <section className="w-full lg:w-[70%] h-[80%] lg:h-screen md:h-screen px-5">
+      <Title title='Experiência' />
+    </section>
+  );
+}
