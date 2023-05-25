81a47d7400d35f94953c1c8cc93cbac24365a869
rafaapcode <rafaap2003@gmail.com>
2023-05-25 09:45:20 -0300

    feat: experience section completed

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -1,78 +1,55 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Title from "../components/title";
-import Image from "next/image";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Experience() {
-  const [selectedId, setSelectedId] = useState(null);
-  const [selectedItem, setSelectedItem] = useState({});
-
   const items = [
     {
       id: 1,
       empresa: "ITP SYSTEMS",
       cargo: "Estagiário de TI",
-      periodo: "Testando outro2",
+      periodo: "Jan de 2023 - Mar de 2023",
+      desc: "Suporte aos usuários da rede e do SAP ( ERP ), manutenção de computadores e instação de softwares, implementação de monitoramento de redes, implementeação de plataforma de ensino ( MOODLE ).",
     },
     {
       id: 2,
       empresa: "ITP SYSTEMS",
       cargo: "Estagiário de TI",
-      periodo: "Testando outro2",
+      periodo: "Jan de 2023 - Mar de 2023",
+      desc: "Suporte aos usuários da rede e do SAP ( ERP ), manutenção de computadores e instação de softwares, implementação de monitoramento de redes, implementeação de plataforma de ensino ( MOODLE ).",
     },
     {
       id: 3,
       empresa: "ITP SYSTEMS",
       cargo: "Estagiário de TI",
-      periodo: "Testando outro2",
+      periodo: "Jan de 2023 - Mar de 2023",
+      desc: "Suporte aos usuários da rede e do SAP ( ERP ), manutenção de computadores e instação de softwares, implementação de monitoramento de redes, implementeação de plataforma de ensino ( MOODLE ).",
     },
   ];
 
   return (
     <section className="w-full lg:w-[70%] h-[80%] lg:h-screen md:h-screen px-5">
       <Title title="Experiência" />
-      <div className="mt-10 w-full flex justify-center md:justify-start lg:justify-start flex-wrap gap-x-2 gap-y-2">
+      <div className="mt-10 w-full h-[70%] lg:h-[75%] flex justify-center md:justify-start lg:justify-start flex-wrap gap-x-2 gap-y-5 overflow-y-auto">
         {items.map((item) => (
           <motion.div
-            className="flex justify-evenly cursor-pointer hover:bg-[#5c5b5b] w-[50%] lg:w-[35%] md:w-[35%] bg-[#1f1d1d] rounded-md shadow px-2 py-3"
-            layoutId={item.id}
-            onClick={() => {
-              setSelectedId(item.id);
-              setSelectedItem(item);
-            }}
+            className="flex flex-col justify-evenly shadow-md shadow-[#2b2727] hover:bg-[#2b2727] cursor-pointer w-full lg:w-[35%] md:w-[60%] md:mx-auto lg:mx-0 bg-[#1f1d1d] rounded-md px-2 py-3"
           >
-            <div className="w-[75%] flex flex-col lg:flex-row lg:justify-around items-center">
-              <motion.p className="text-base">{item.empresa}</motion.p>
-              <motion.p className="text-sm text-[#494949] ">
+            <div className="w-full flex flex-col lg:flex-row lg:justify-around items-center">
+              <motion.p className="text-base md:text-lg">{item.empresa}</motion.p>
+              <motion.p className="text-sm md:text-base text-[#494949]">
                 {item.cargo}
               </motion.p>
             </div>
+            <motion.p className="mb-2 mt-2 lg:mt-0 self-center text-xs px-1 text-[#818080]">
+              {item.periodo}
+            </motion.p>
+            <div className="mx-auto w-1/2 h-[1px] bg-[#3a3a3a]"></div>
+            <p className="mt-2 px-2 text-xs md:text-sm">{item.desc}</p>
           </motion.div>
         ))}
-
-        <AnimatePresence>
-          {selectedId && (
-            <motion.div
-              layoutId={selectedId}
-              className="w-fit h-fit flex flex-col justify-center items-center"
-            >
-              <motion.button
-                className="bg-slate-500 w-[35px] h-[35px] self-end rounded-full"
-                onClick={() => {
-                  setSelectedId(null);
-                  setSelectedItem({});
-                }}
-              >
-                X
-              </motion.button>
-              <div> 
-                <h1>OLá sou o rafa</h1>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
       </div>
     </section>
   );
