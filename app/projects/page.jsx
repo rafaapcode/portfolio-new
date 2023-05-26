c413fc7bd8e701d32287b1d7765a30c7dc2e35d7
rafaapcode <rafaap2003@gmail.com>
2023-05-26 09:20:12 -0300

    feat: projects section complete

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -10,15 +10,15 @@ export default function Projects() {
   const [selectedItem, setSelectedItem] = useState({});
 
   const items = [
-    { id: 1, nome: "Reservas", desc: "teste do teste", techs: ["HTML", "CSS", "JS", "PY", "C", "JAVA"] },
-    { id: 2, nome: "Cadastro", desc: "teste do teste", techs: ["JS", "PY"] },
-    { id: 3, nome: "Futebol APP", desc: "teste do teste", techs: ["C", "JAVA"] },
+    { id: 1, nome: "Reservas", desc: "teste do teste", techs: ["HTML", "CSS", "JS", "PY", "C", "JAVA"], desc: 'Sistema para reservas em sala de aula. Ele me da o caminho mais rápido que posso fazer as reservas, gastando menos tempo.' },
+    { id: 2, nome: "Cadastro", desc: "teste do teste", techs: ["JS", "PY"], desc: 'testando' },
+    { id: 3, nome: "Futebol APP", desc: "teste do teste", techs: ["C", "JAVA"], desc: 'testando' },
   ];
 
   return (
     <section className="w-full lg:w-[70%] h-[80%] lg:h-screen md:h-screen px-5 overflow-auto">
       <Title title="Projetos" />
-      <div className="mt-10 w-full flex gap-x-2 h-[50%]">
+      <div className="mt-10 w-full flex gap-x-2 h-[50%] relative">
         {items.map((item) => (
           <motion.div
             className="mt-10 p-2 bg-[#201f1f] w-[35%] h-[35%] rounded-md cursor-pointer"
@@ -38,14 +38,14 @@ export default function Projects() {
         ))}
         <AnimatePresence>
           {selectedId && (
-            <motion.div className="bg-red-300" layoutId={selectedId}>
-              <motion.h5>{selectedItem.nome}</motion.h5>
+            <motion.div className="backdrop-blur flex flex-col left-[20%] rounded w-[60%] h-[60%] absolute bg-[#666565]" layoutId={selectedId}>
               <motion.button
-                className="bg-orange-300"
+                className="self-end mr-2 mt-2 text-center transition-all duration-75 hover:bg-[#505050] bg-[#a09f9f] h-[30px] w-[30px] rounded-full"
                 onClick={() => setSelectedId(null)}
               >
-                exit
+                X
               </motion.button>
+              <motion.p className="px-2 font-bold text-[#fff]">{selectedItem.desc}</motion.p>
             </motion.div>
           )}
         </AnimatePresence>
