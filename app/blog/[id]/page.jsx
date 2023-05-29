b85c6915b7b573e04a8ba9c8fb6a3033534e7254
rafaapcode <rafaap2003@gmail.com>
2023-05-29 09:34:36 -0300

    feat: complete the blog section

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,11 +1,37 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Posts({params}) {
+export default function Posts({ params }) {
+  const post = {
+    desc: "<h2><b>Testando</b>O Teste</h2><br/>",
+  };
   return (
-    <motion.section initial={{ scale: 0 }} animate={{scale: 1}} className="mt-10 w-full lg:w-[70%] h-[80%] lg:h-[90%] md:h-screen px-5 bg-red-300">
-        Como faço uma rota dinâmica no {params.id}
+    <motion.section
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      className="mt-10 w-full lg:w-[70%] h-[80%] lg:h-[90%] md:h-screen px-5"
+    >
+      <motion.div
+        whileHover={{ scale: 1.05 }}
+        className="w-fit h-fit cursor-pointer"
+      >
+        <Link href="/">
+          <Image
+            src="/images/arrow.png"
+            width={50}
+            height={50}
+            alt="icon arrow back"
+          />
+        </Link>
+      </motion.div>
+      <div
+        className="mt-5 w-full px-5 text-lg flex flex-col items-center h-[85%] overflow-auto gap-y-2"
+        dangerouslySetInnerHTML={{ __html: post.desc }}
+      >
+      </div>
     </motion.section>
   );
 }
