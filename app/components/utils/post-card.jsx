0d32bd313ec3f3735a988837c734a92180ea5037
rafaapcode <rafaap2003@gmail.com>
2023-06-10 18:14:15 -0300

    fix: the blog page was not appearing in the deploy

--- app/components/utils/post-card.jsx
+++ app/components/utils/post-card.jsx
@@ -7,7 +7,7 @@ export default function PostsCard({ id, titulo, techs }) {
   return (
     <div className="w-full">
       <div className="mt-5 max-w-full lg:max-w-[45%] md:ml-7 lg:ml-7 px-5 py-1 bg-[#2c2929] rounded-lg">
-        <a href={`/posts/${id}`} target="_blank">
+        <a href={`/${id}`} target="_blank">
           <h1 className="text-white text-lg">{titulo}</h1>
         </a>
         <div className="flex flex-wrap gap-x-2 gap-y-2 text-[#fff] p-3">
