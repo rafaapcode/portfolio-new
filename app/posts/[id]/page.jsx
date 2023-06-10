862c7e7cbd55f7b70810c1fb05b354ba0db85a75
rafaapcode <rafaap2003@gmail.com>
2023-06-10 17:36:02 -0300

    feat: finish the design of blog page

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -10,16 +10,16 @@ export default function Posts({ params }) {
 
   return (
     <div className="w-screen h-screen bg-[#111010]">
-      <div className="ml-[10%] text-white w-fit p-5 hover:text-[#919191]">
-        <Link href="/">VOLTAR</Link>
-      </div>
       <div className="w-full p-2 flex flex-col justify-center items-center italic">
-        <p className="text-4xl text-white">{post.titulo}</p>
-        <div className="mt-5 flex gap-x-2 gap-y-2 max-w-[30%] p-2 w-fit">
+        <p className="mt-5 text-4xl text-white">{post.titulo}</p>
+        <div className="mt-5 flex gap-x-2 gap-y-2 max-w-full md:max-w-[50%] lg:max-w-[30%] p-2 w-fit">
           {post.tags.map((techs, index) => (
             <Tags key={index} techname={techs} />
           ))}
         </div>
+      </div>
+      <div className="text-white mx-auto w-[95%] h-[77%] overflow-auto">
+        <h1>Todo o content</h1>
       </div>
     </div>
   );
