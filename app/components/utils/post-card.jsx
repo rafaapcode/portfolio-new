30befbd2e68f7fd7a671fe37f6b7d3ebf9405a9d
rafaapcode <rafaap2003@gmail.com>
2023-06-10 08:11:06 -0300

    feat: redirecting to Post page

--- app/components/utils/post-card.jsx
+++ app/components/utils/post-card.jsx
@@ -7,9 +7,9 @@ export default function PostsCard({ id, titulo, techs }) {
   return (
     <div className="w-full">
       <div className="mt-5 max-w-full lg:max-w-[45%] ml-7 px-5 py-1 bg-[#2c2929] rounded-lg">
-        <button>
+        <a href={`/${id}`}>
           <h1 className="text-white text-lg">{titulo}</h1>
-        </button>
+        </a>
         <div className="flex flex-wrap gap-x-2 gap-y-2 text-[#fff] p-3">
           {techs.map((tech, index) => (
             <span className="px-3 text-[#888585] bg-[#464141] rounded-md">
