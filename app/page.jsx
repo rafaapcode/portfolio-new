661720a0b4736cdcb1cd31c5dfc1d79b67697559
rafaapcode <rafaap2003@gmail.com>
2023-06-11 08:19:33 -0300

    feat: change the design of blog button

--- app/page.jsx
+++ app/page.jsx
@@ -56,7 +56,7 @@ export default function Header() {
               animate={{ scale: 1 }}
               whileTap={{ y: -5 }}
               transition={{ duration: 0.4 }}
-              className="w-fit h-fit px-2 py-1 rounded-md mt-2 bg-[#262626] hover:cursor-pointer text-[#E5E5E5] transition ease-in-out duration-500"
+              className="w-fit h-fit px-2 py-1 rounded-md mt-2 hover:cursor-pointer hover:text-[#838383]  text-[#E5E5E5] transition ease-in-out duration-500"
             >
               <a href="/blog">Blog</a>
             </motion.li>
