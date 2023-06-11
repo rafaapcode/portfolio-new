fb40aa13aa07a62c1a74fc31a40610dad279e0e5
rafaapcode <rafaap2003@gmail.com>
2023-06-11 09:49:48 -0300

    feat: creating the form contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,55 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Contact() {
+  const [nome, setNome] = useState("");
+  const [assunto, setAssunto] = useState("");
+  const [mensagem, setMensagem] = useState("");
+  const [contact, setContact] = useState({});
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setContact({ nome, assunto, mensagem });
+    setAssunto('');
+    setNome('');
+    setMensagem('');
+  }
+
   return (
-    <div className="w-full h-screen text-white bg-[#111010]">
-      <h1>Teste</h1>
+    <div className="w-full h-screen flex justify-center items-center text-white bg-[#111010]">
+      <form
+        className="w-[40%] h-[65%] flex flex-col justify-center items-center gap-y-5 p-5 border backdrop-blur-md rounded-lg border-[#1d1b1b] shadow-lg"
+        onSubmit={handleSubmit}
+      >
+        <div className="w-[80%] flex justify-center items-center gap-x-5">
+          <span>Nome : </span>
+          <input
+            onChange={(e) => setNome(e.target.value)}
+            value={nome}
+            className="rounded-md outline-none bg-[#1d1b1b] p-1"
+            type="text"
+          />
+        </div>
+        <div className="w-[80%] flex justify-center items-center gap-x-2">
+          <span>Assunto : </span>
+          <input
+            onChange={(e) => setAssunto(e.target.value)}
+            value={assunto}
+            className="rounded-md outline-none bg-[#1d1b1b] p-1"
+            type="text"
+          />
+        </div>
+        <div className="w-[80%] h-[70%] flex flex-col justify-center items-center gap-x-2 gap-y-2">
+          <span>Mensagem : </span>
+          <textarea
+            onChange={(e) => setMensagem(e.target.value)}
+            value={mensagem}
+            className="rounded-md w-[80%] h-full outline-none bg-[#1d1b1b] p-1"
+          />
+        </div>
+        <button className="cursor-pointer bg-[#2c2929] hover:bg-[#3b3737] rounded-md px-3 py-1" type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {children}
+      <body>{children}</body>
     </html>
   );
 }
